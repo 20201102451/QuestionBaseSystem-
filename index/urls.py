56575798f72ada60eb798b83553e8a49d14cebf8
Labio07 <1348757279@qq.com>
2022-10-17 22:36:58 +0800

    4th commit by Fan

--- index/urls.py
+++ index/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('subjectDesc/',views.subjectDesc),
-    path('personalCenter/',views.personalCenter),
+    path('subjectDesc/', views.subjectDesc),
+    path('personalCenter/', views.personalCenter),
+    path('personalCenter2/', views.personalCenter2),
+    path('', views.index),
 ]
--- index/views.py
+++ index/views.py
@@ -6,4 +6,7 @@ def subjectDesc(request):
     return render(request, 'subjectDesc.html')
 def personalCenter(request):
     return render(request, 'PersonalCenter.html')
-
+def personalCenter2(request):
+    return render(request, 'PersonalCenter2.html')
+def index(request):
+    return render(request, 'index.html')
